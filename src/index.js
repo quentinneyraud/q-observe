95098169f682606b54d89b493147ab7cc45b780a
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-10-07 18:18:28 +0200

    limit exposed functions

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
 // import ObservedElement from './ObservedElement'
 import { preprocessElementArgument } from './utils'
 
-export default {
-  observers: [],
-  elements: [],
-  observe (el = null, options) {
-    if (!el) {
-      console.warn('Element argument passed to observe function is not valid')
-      return
+const observe = (el = null, options) => {
+  if (!el) {
+    console.warn('Element argument passed to observe function is not valid')
+    return
+  }
+
+  console.log(preprocessElementArgument(el))
+}
+
+const observeChildren = (parentEl = null, options = {}) => {
+  if (!parentEl) {
+    console.warn('Parent element argument passed to observeChildren function is not valid')
+    return
+  }
+
+  const elements = preprocessElementArgument(parentEl)
+
+  elements
+    .forEach(el => observe(el))
+
+  if (options.watch) {
+    const config = {
+      attributes: false,
+      childList: true,
+      characherData: false,
+      subtree: true
     }
 
-    console.log(preprocessElementArgument(el))
-  },
+    const callback = (mutationsList) => {
+      for (const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          console.log('mutation:', mutation)
+          Array.from(mutation.addedNodes)
+            .forEach(node => observe(node))
+        }
+      }
+    }
+
+    const observer = new MutationObserver(callback)
+    elements
+      .forEach(el => observer.observe(el, config))
+  }
+}
+
+const obj = {
   observeOnce () {
 
   },
   unobserve () {
 
-  },
-  observeChildren (parentEl = null, options = {}) {
-    if (!parentEl) {
-      console.warn('Parent element argument passed to observeChildren function is not valid')
-      return
-    }
-
-    const elements = preprocessElementArgument(parentEl)
-
-    elements
-      .forEach(el => this.observe(el))
-
-    if (options.watch) {
-      const config = {
-        attributes: false,
-        childList: true,
-        characherData: false,
-        subtree: true
-      }
-
-      const callback = (mutationsList) => {
-        for (const mutation of mutationsList) {
-          if (mutation.type === 'childList') {
-            console.log('mutation:', mutation)
-            Array.from(mutation.addedNodes)
-              .forEach(node => this.observe(node))
-          }
-        }
-      }
-
-      const observer = new MutationObserver(callback)
-      elements
-        .forEach(el => observer.observe(el, config))
-    }
   },
   unobserveChildren () {
 
@@ -73,4 +73,9 @@ export default {
   getObserver () {
 
   }
+}
+
+export default {
+  observe,
+  observeChildren
 }
